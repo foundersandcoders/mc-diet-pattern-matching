f38482482b54f7b90f0dea19e7ccf7b1f31e3fe4
finnhodgkin <finnhodgkin@gmail.com>
2018-01-17 17:07:57 +0000

    adds some simpler early exercises

--- index.js
+++ index.js
@@ -1,67 +1,138 @@
-const greetObj = {
-  hello: 'hello',
-  there: { there: 'there', pal: ['nope', 'nope', 'pal'] },
+// Here is an example of the data you need to destructure
+// const simpleArray = [1, 2];
+//
+const multiplyArray = (/* Only edit between these perens */) => {
+  console.log(a * b);
+  return a * b;
 };
 
+// Here is your data type. Only destructure the dinosaurs. No fish.
+// const animalObj = [
+//   'Deer',
+//   'Allosaurus',
+//   'Dog',
+//   'Brachiosaurus',
+//   'Fish',
+//   'Diplodocus',
+// ];
+//
+const captureJustDinosaurs = (/* Only edit between these perens */) => {
+  const dinoString = `${Allosaurus} ${Brachiosaurus} ${Diplodocus}`;
+  console.log(dinoString);
+  return dinoString;
+};
+
+// This time only destructure the cuckoo chicks and rename them cookoo1, etc.
+// const removeTheCuckoo = {
+//   Nest1: {
+//     chick1: 'tweet',
+//     chick2: 'tweet',
+//     chick3: 'tweet',
+//     chick4: 'cuckoo',
+//   },
+//   Nest2: {
+//     chick1: 'caaaaaaw',
+//     chick2: 'cuckoo',
+//     chick3: 'caaaaw',
+//     chick4: 'caaaaaw',
+//   },
+//   Nest3: {
+//     chick1: 'SQUAAAAAAAA',
+//     chick2: 'cuckoo',
+//     chick3: 'SQUAAAAAAAA',
+//     chick4: 'SQUAAAAAAAA',
+//   },
+// };
+//
+const nestedBirds = (/* Only edit between these perens */) => {
+  console.log(`${cuckoo1} ${cuckoo2} ${cuckoo3}`);
+  return `${cuckoo1} ${cuckoo2} ${cuckoo3}`;
+};
+
+// This time we'll need to add default values for the undefined colours.
+// const coloursOfTheRainbow = [
+//   'red',
+//   undefined,
+//   undefined,
+//   'green',
+//   undefined,
+//   undefined,
+//   'violet',
+// ];
+//
+const listEverySingleColour = (/* Only change this bit :) */) => {
+  const cls = `${red} ${orange} ${yellow} ${green} ${blue} ${purple} ${violet}`;
+  console.log(cls);
+  return cls;
+};
+
+// You're on your own from here on out :)
+//
+//
+// const greetObj = {
+//   hello: 'hello',
+//   there: { there: 'there', pal: ['nope', 'nope', 'pal'] },
+// };
+//
 const greet = (/* Only edit within the brackets to fix the function! */) => {
   console.log(`${hello} ${there} ${pal}`);
   return `${hello} ${there} ${pal}`;
 }; // 'hello there pal'
 
-const hummusReviewObj = {
-  level1: {
-    level2: {
-      is: [null, undefined, false, 0, '', 'is'],
-      level3: {
-        level3andABit: {
-          funkshun: (a, b) => a + b,
-        },
-        level4: {
-          mus: 'hum',
-        },
-      },
-      hu: 'mus',
-    },
-  },
-  is: 'life',
-};
-
+// const hummusReviewObj = {
+//   level1: {
+//     level2: {
+//       is: [null, undefined, false, 0, '', 'is'],
+//       level3: {
+//         level3andABit: {
+//           funkshun: (a, b) => a + b,
+//         },
+//         level4: {
+//           mus: 'hum',
+//         },
+//       },
+//       hu: 'mus',
+//     },
+//   },
+//   is: 'life',
+// };
+//
 const hummusReview = (/* Play with the arguments to review hummus */) => {
   console.log(`${concat(hum, mus)} ${is} ${life}`);
   return `${concat(hum, mus)} ${is} ${life}`;
 }; // 'hummus is life'
 
-const catArr = [
-  "there's",
-  {
-    a: 'cat',
-    on: [
-      null,
-      'balcony',
-      {
-        nothing: ['on'],
-      },
-      'my',
-      [[[['nope'], ['a']]]],
-    ],
-  },
-];
-
+// const catArr = [
+//   "there's",
+//   {
+//     a: 'cat',
+//     on: [
+//       null,
+//       'balcony',
+//       {
+//         nothing: ['on'],
+//       },
+//       'my',
+//       [[[['nope'], ['a']]]],
+//     ],
+//   },
+// ];
+//
 const cat = (/* Cat-based destructuring */) => {
   console.log([one, two, three, four, five, six].reverse().join(' '));
   return [one, two, three, four, five, six].reverse().join(' ');
 }; // 'There's a cat on my balcony'
 
-const defaultsArray = [
-  'one',
-  undefined,
-  null,
-  'four',
-  undefined,
-  'six',
-  { seven: { no: undefined } },
-];
-
+// const defaultsArray = [
+//   'one',
+//   undefined,
+//   null,
+//   'four',
+//   undefined,
+//   'six',
+//   { seven: { no: undefined } },
+// ];
+//
 const defaults = (/* Destructuring with defaults! */) => {
   console.log(`${one} ${two} ${three} ${four} ${five} ${six} ${seven}`);
   return `${one} ${two} ${three} ${four} ${five} ${six} ${seven}`;
@@ -72,4 +143,8 @@ module.exports = {
   hummusReview,
   cat,
   defaults,
+  multiplyArray,
+  captureJustDinosaurs,
+  nestedBirds,
+  listEverySingleColour,
 };
--- index.test.js
+++ index.test.js
@@ -1,5 +1,121 @@
 const test = require('tape');
-const { greet, hummusReview, cat, defaults } = require('./index');
+const {
+  greet,
+  hummusReview,
+  cat,
+  defaults,
+  multiplyArray,
+  captureJustDinosaurs,
+  nestedBirds,
+  listEverySingleColour,
+} = require('./index');
+
+const addFixture1 = [1, 2];
+const addFixture2 = [3, 4];
+
+test('multiplyArray yay', ({ end, equal }) => {
+  equal(multiplyArray(addFixture1), 2, 'Multiplied the array, woo!');
+  equal(multiplyArray(addFixture2), 12, 'Multiplied the bigger numbers, woo!');
+  end();
+});
+
+const dinoFixture1 = [
+  'Deer',
+  'Allosaurus',
+  'Dog',
+  'Brachiosaurus',
+  'Fish',
+  'Diplodocus',
+];
+const dinoFixture2 = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+test('Just the dinosaurs', ({ end, equal }) => {
+  equal(
+    captureJustDinosaurs(dinoFixture1),
+    'Allosaurus Brachiosaurus Diplodocus',
+    'We got dinosaurs only'
+  );
+  equal(captureJustDinosaurs(dinoFixture2), 'b d f', 'Only some letters!');
+  end();
+});
+
+const birdsFixture = {
+  Nest1: {
+    chick1: 'tweet',
+    chick2: 'tweet',
+    chick3: 'tweet',
+    chick4: 'cuckoo',
+  },
+  Nest2: {
+    chick1: 'caaaaaaw',
+    chick2: 'cuckoo',
+    chick3: 'caaaaw',
+    chick4: 'caaaaaw',
+  },
+  Nest3: {
+    chick1: 'SQUAAAAAAAA',
+    chick2: 'cuckoo',
+    chick3: 'SQUAAAAAAAA',
+    chick4: 'SQUAAAAAAAA',
+  },
+};
+const boringStringsFixture = {
+  Nest1: {
+    chick1: '',
+    chick2: '',
+    chick3: '',
+    chick4: 'BLAH',
+  },
+  Nest2: {
+    chick1: '',
+    chick2: 'BLAH',
+    chick3: '',
+    chick4: '',
+  },
+  Nest3: {
+    chick1: '',
+    chick2: 'BLAH',
+    chick3: '',
+    chick4: '',
+  },
+};
+test('Kicked out those cuckoos', ({ end, equal }) => {
+  equal(nestedBirds(birdsFixture), 'cuckoo cuckoo cuckoo', 'Get em!');
+  equal(nestedBirds(boringStringsFixture), 'BLAH BLAH BLAH', 'Blah!');
+  end();
+});
+
+const colourFixture = [
+  'red',
+  undefined,
+  undefined,
+  'green',
+  undefined,
+  undefined,
+  'violet',
+];
+const boringNumberFixture = [
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+];
+test('Did some rainbow default stuff', ({ end, equal }) => {
+  equal(
+    listEverySingleColour(colourFixture),
+    'red orange yellow green blue purple violet',
+    'Oooo pretty!'
+  );
+  equal(
+    listEverySingleColour(boringNumberFixture),
+    'one two three four five six seven',
+    'Numbers!'
+  );
+  end();
+});
 
 const greetFixture1 = {
   hello: 'hello',
